064e65e85167124d1535319c92605923f833ced0
yamamoto nodoka <mild.summer.y@gmail.com>
2015-04-21 16:22:33 +0900

    heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var server = require('http').Server(app);
+//var server = require('http').Server(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -57,52 +57,52 @@ app.use(function(err, req, res, next) {
   });
 });
 
-var port = process.env.PORT || '3000';
-var io = require('socket.io').listen(server);
-
-server.listen(port);
-
-console.log((new Date()) + " Server is listening on port " + port);
-
-//設定
-// io.configure(function () {
-//    //HerokuではWebSocketがまだサポートされていない？ので、以下の設定が必要
-//     io.set("transports", ["xhr-polling"]);
-//     io.set("polling duration", 10);
+//var port = process.env.PORT || '3000';
+//var io = require('socket.io').listen(server);
 //
-//     // socket.ioのログ出力を抑制する
-//     io.set('log level', 1);
-// });
-
-io.on('connection', function(socket) {
-  // 入室
-  socket.on('enter', function(roomname) {
-      socket.roomname = roomname;
-      //socket.set('roomname', roomname);
-      socket.join(roomname);
-  });
-
-  socket.on('message', function(message) {
-    emitMessage('message', message);
-  });
-
-  socket.on('disconnect', function() {
-    emitMessage('user disconnected');
-  });
-
-  // 会議室名が指定されていたら、室内だけに通知
-  function emitMessage(type, message) {
-    //var roomname;
-    //socket.get('roomname', function(err, _room) {  roomname = _room;  });
-
-    if (socket.roomname) {
-      socket.broadcast.to(socket.roomname).emit(type, message);
-      console.log("room:" + socket.roomname);
-    } else {
-      socket.broadcast.emit(type, message);
-      console.log("no room");
-    }
-  }
-});
+//server.listen(port);
+//
+//console.log((new Date()) + " Server is listening on port " + port);
+//
+////設定
+//// io.configure(function () {
+////    //HerokuではWebSocketがまだサポートされていない？ので、以下の設定が必要
+////     io.set("transports", ["xhr-polling"]);
+////     io.set("polling duration", 10);
+////
+////     // socket.ioのログ出力を抑制する
+////     io.set('log level', 1);
+//// });
+//
+//io.on('connection', function(socket) {
+//  // 入室
+//  socket.on('enter', function(roomname) {
+//      socket.roomname = roomname;
+//      //socket.set('roomname', roomname);
+//      socket.join(roomname);
+//  });
+//
+//  socket.on('message', function(message) {
+//    emitMessage('message', message);
+//  });
+//
+//  socket.on('disconnect', function() {
+//    emitMessage('user disconnected');
+//  });
+//
+//  // 会議室名が指定されていたら、室内だけに通知
+//  function emitMessage(type, message) {
+//    //var roomname;
+//    //socket.get('roomname', function(err, _room) {  roomname = _room;  });
+//
+//    if (socket.roomname) {
+//      socket.broadcast.to(socket.roomname).emit(type, message);
+//      console.log("room:" + socket.roomname);
+//    } else {
+//      socket.broadcast.emit(type, message);
+//      console.log("no room");
+//    }
+//  }
+//});
 
 module.exports = app;
